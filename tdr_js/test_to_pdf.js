d6c3c5bc1573c19b2ff79823c98633d984d95747
whiprandolph <wsr.business@gmail.com>
2025-10-14 16:34:20 -0400

    added test chapter node file

---
+++ tdr_js/test_to_pdf.js
@@ -0,0 +1,28 @@
+
+const puppeteer = require('puppeteer');
+
+(async() => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8000/test_chapter.html', {waitUntil: 'networkidle2'});
+  await page.pdf({
+    displayHeaderFooter: true,
+    width: '6in',
+    height: '9in',
+    preferCSSPageSize: true,
+    printBackground: true,
+    headerTemplate: '<div id="header-template"></div>',
+    footerTemplate: '<div id="footer-template" style="font-size:11px !important; color:#000000; position:absolute; left:.55in; top:8.65in;"> <span class="pageNumber"></span> </div>',
+    path:'C:\\Users\\whip\\tdr_published_files\\tmp_dir\\test_chapter.pdf',
+    margin: {
+      top: '0px',
+      bottom: '0px',
+      right: '0px',
+      left: '0px',
+    },
+  });
+
+  await browser.close();
+})();
+
+
